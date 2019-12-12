580cc316254c1da1e994e9999c7f43696517d851
Jofferson R. Tiquez <jofftiquez@gmail.com>
2019-12-13 02:11:19 +0800

    refactor: re-structure project

---
+++ src/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app dark>
+    <v-content>
+      <v-cotainer>
+        <nuxt />
+      </v-cotainer>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <v-card>
+        <v-card-text>
+          <h1>Hello! This is server side rendered!</h1>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {}
+</script>
